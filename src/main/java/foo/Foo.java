01219918c76ca3bdfde471234d082e9db7674c7f
jamesflynn <jamesflynn@192.168.12.29>
2019-02-08 17:35:36 +0100

    Passes Rule 4.1.1 if flag numbers containing 3 with "Foo" Flag numbers disible by 3 with "Foo"

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -3,15 +3,25 @@ package foo;
 public class Foo {
 
 	public String compute(int input) {
+		String decInput = Integer.toString(input);
+		StringBuilder output = new StringBuilder();
 		if (input % 3 == 0) {
-			return "Foo";
+			output.append("Foo");
 		}
 		if (input % 5 == 0) {
-			return "Bar";
+			output.append("Bar");
 		}
 		if (input % 7 == 0) {
-			return "Qix";
+			output.append("Qix");
 		}
-		return Integer.toString(input);
+		for (char ch : decInput.toCharArray()) {
+			if (ch == '3') {
+				output.append("Foo");
+			}
+		}
+		if (output.length() == 0) {
+			output.append(decInput);
+		}
+		return output.toString();
 	}
 }
