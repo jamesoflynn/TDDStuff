da7e657d3a8d0b1a1372051be216e0345f9a46d6
jamesflynn <jamesflynn@192.168.12.29>
2019-02-08 17:00:57 +0100

    added test for flagging numbers divisible by 5.

--- src/test/java/foo/FooTest.java
+++ src/test/java/foo/FooTest.java
@@ -21,9 +21,15 @@ public class FooTest {
 	}
 
 
-	@Test
+	@Test // Rule 1 flag number divisible by 3.
 	public void isDivisibleByThree() {
 		String output = foo.compute(3);
 		Assert.assertTrue(output.contains("Foo"));
+	}
+
+	@Test // Rule 2 flag number divisible by 5.
+	public void isDivisibleByFive() {
+		String output = foo.compute(5);
+		Assert.assertTrue(output.contains("Bar"));
 	}
 }
