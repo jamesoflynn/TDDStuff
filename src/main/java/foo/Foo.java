c865f22159f27cf184c5cdbebe5f9f84ef939d51
jamesflynn <jamesflynn@192.168.12.29>
2019-02-08 19:00:54 +0100

    Passes test 5.2. Todo cases of interest.

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -5,28 +5,38 @@ public class Foo {
 	public String compute(int input) {
 		String decInput = Integer.toString(input);
 		StringBuilder output = new StringBuilder();
+		boolean isfbq = false;
 		if (input % 3 == 0) {
 			output.append("Foo");
+			isfbq = true;
 		}
 		if (input % 5 == 0) {
 			output.append("Bar");
+			isfbq = true;
 		}
 		if (input % 7 == 0) {
 			output.append("Qix");
+			isfbq = true;
 		}
 		for (char ch : decInput.toCharArray()) {
 			if (ch == '3') {
 				output.append("Foo");
+				isfbq = true;
 			} else if (ch == '5') {
 				output.append("Bar");
+				isfbq = true;
 			} else if (ch == '7') {
 				output.append("Qix");
+				isfbq = true;
 			} else if (ch == '0') {
 				output.append("*");
 			}
 		}
-		if (output.length() == 0) {
-			output.append(decInput);
+		if (!isfbq) {
+			output.setLength(0);
+			for (char ch : decInput.toCharArray()) {
+				output.append(ch == '0' ? '*' : ch);
+			}
 		}
 		return output.toString();
 	}
